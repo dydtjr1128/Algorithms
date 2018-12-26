b247bd4eb412a861e645512f1d3998e92ccde106
dydtjr1128 <cys_star@naver.com>
2018-12-26 14:03:36 +0900

    Solve Algorithms

--- EX-피보나치 퍼포먼스(for,recursion)/src/Main.java
+++ EX-피보나치 퍼포먼스(for,recursion)/src/Main.java
@@ -3,17 +3,10 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 
 public class Main {
-    private static int zeroCount = 0;
-    private static int oneCount = 0;
-    private static int array[] = new int[41];
-    private static int array2[] = new int[41];
-
     public static int fibonacci(int n) {
         if (n == 0) {
-            zeroCount++;
             return 0;
         } else if (n == 1) {
-            oneCount++;
             return 1;
         } else {
             return fibonacci(n - 1) + fibonacci(n - 2);
@@ -23,10 +16,8 @@ public class Main {
     public static int fibonacci2(int n) {
         int sum = 0;
         if (n == 0) {
-            zeroCount++;
             sum = n;
         } else if (n == 1) {
-            oneCount++;
             sum = n;
         } else {
             int temp1 = 1, temp2 = 0;
@@ -40,22 +31,22 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
+        final int TEST_NUM = 45;
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        writer.write("Fibonacci Recursion start!!\n");
+        writer.write(TEST_NUM + "th fibonacci number.\n");
+        writer.write("Fibonacci recursion start!!\n");
         long st = System.nanoTime();
-        for (int i = 0; i < 45; i++) {
+        for (int i = 0; i < TEST_NUM; i++) {
             fibonacci(i);
-            zeroCount = oneCount = 0;
         }
         writer.write(String.format("%.5fms", (System.nanoTime() - st) / (double) 1000000));
         writer.newLine();
         writer.flush();
 
-        writer.write("Fibonacci For loop start!!\n");
+        writer.write("Fibonacci for-loop start!!\n");
         st = System.nanoTime();
-        for (int i = 0; i < 45; i++) {
+        for (int i = 0; i < TEST_NUM; i++) {
             fibonacci2(i);
-            zeroCount = oneCount = 0;
         }
         writer.write(String.format("%.5fms", (System.nanoTime() - st) / (double) 1000000));
         writer.newLine();
