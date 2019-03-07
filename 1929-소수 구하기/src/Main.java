1677abab3716a9ad94d79422f8800259be55e265
dydtjr1128 <cys_star@naver.com>
2019-03-07 16:20:27 +0900

    Solve Algorithms

--- 1929-소수 구하기/src/Main.java
+++ 1929-소수 구하기/src/Main.java
@@ -1,34 +1,38 @@
+import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner S = new Scanner(System.in);
-		boolean sosu[] = new boolean[1000001];
-		int min, max;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        boolean sosu[] = new boolean[1000001];
+        int min, max;
+        String temp[] = reader.readLine().split(" ");
+        min = Integer.parseInt(temp[0]);
+        max = Integer.parseInt(temp[1]) + 1;
 
-		min = S.nextInt();
-		max = S.nextInt() + 1;
+        Arrays.fill(sosu, true);
+        sosu[0] = false;
+        sosu[1] = false;    //1ë„ ì†Œìˆ˜ê°€ ì•„ë‹˜
+        for (int i = 2; i < max; i++) {
+            if (!sosu[i])
+                continue;
+            else {
+                for (int j = i * 2; j < max; j += i) {
+                    sosu[j] = false;
+                }
+            }
+        }
+        for (int i = min; i < max; i++) {
+            if (sosu[i]) {
+                writer.write(String.valueOf(i));
+                writer.newLine();
+            }
+        }
 
-		Arrays.fill(sosu, true);	
-		sosu[0]=false;
-		sosu[1]=false;	//1µµ ¼Ò¼ö°¡ ¾Æ´Ô
-		for (int i = 2; i < max; i++) {
-			if (!sosu[i])
-				continue;
-			else {
-				for (int j = i * 2; j < max; j += i) {
-					sosu[j] = false;
-				}
-			}
-		}
-		for (int i = min; i < max; i++) {
-			if (sosu[i]) {
-				System.out.println(i);
-			}
-		}
-
-		S.close();
-	}
+        writer.flush();
+        writer.close();
+    }
 }
