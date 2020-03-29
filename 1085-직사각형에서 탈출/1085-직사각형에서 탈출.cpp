ba058022ebd76486e23ebc335d7b63bd465deb3a
Choi Yong Seok <dydtjr1994@gmail.com>
2020-03-29 21:53:42 +0900

    Add algorithms

---
+++ 1085-직사각형에서 탈출/1085-직사각형에서 탈출.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main() {
+	int x, y, w, h;
+	int max = w > h ? w : h ;
+	std::cin >> x >> y >> w >> h;
+	if (max > x) max = x;
+	if (max > y) max = y;
+	if (max > w-x) max = w-x;
+	if (max > h-y) max = h-y;
+	std::cout << max;
+}
---
+++ 3009-네 번째 점/3009-네 번째 점.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main() {
+	int x1, x2, x3, x4, y1, y2, y3, y4;
+	std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+	if (x1 == x2) {
+		x4 = x3;
+		if (y3 == y1) {
+			y4 = y2;
+		}
+		else { // y3 == y2
+			y4 = y1;
+		}
+	}
+	else { // x2 == x3
+		x4 = x1;
+		if (y1 == y2) {
+			y4 = y3;
+		}
+		else { // y1 == y3
+			y4 = y2;
+		}
+	}
+
+
+	std::cout << x4 << " " << y4;
+
+}
