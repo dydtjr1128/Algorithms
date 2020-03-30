e87b4fe76386923b2cd7af8361679b9d5a1e830e
Choi Yong Seok <dydtjr1994@gmail.com>
2020-03-30 22:57:13 +0900

    Add algorithms

---
+++ 3053-택시 기하학/3053-택시 기하학.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+
+
+//원의 정의는 한 점에서 같은 거리에 놓인 점들을 의미한다. => 다이아몬드꼴
+int main() {
+	int r;
+	std::cin >> r;
+	double pi = 3.1415926535897931;
+
+	double a1 = pi * r * r;
+	double a2 = 2.0 * r * r;
+
+	std::cout << std::fixed;
+	std::cout.precision(6);
+	std::cout << a1 << std::endl;
+	std::cout << a2;
+}
---
+++ 4153-직각삼각형/4153-직각삼각형.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main() {
+	int a, b, c;
+	int max, t1, t2;
+	while (true) {
+		std::cin >> a >> b >> c;
+		if (a == 0 && b == 0 && c == 0)
+			break;
+		else {
+			if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a)) {
+				std::cout << "right" << std::endl;
+			}
+			else {
+				std::cout << "wrong" << std::endl;
+			}
+		}
+	}
+}
