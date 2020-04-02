9d57fd65b4993c12aa61bc4c2c12ab6aa1931584
Choi Yong Seok <dydtjr1994@gmail.com>
2020-04-02 21:42:29 +0900

    Add algorithms

---
+++ 10870-피보나치 수 5/10870-피보나치 수 5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int n, m = 0;
+
+int fibo(int a, int b) {
+	++m;
+	if (m == n)
+		return a + b;
+
+	return fibo(a + b, a);
+}
+
+int main() {	
+	std::cin >> n;
+	if (n == 0) {
+		std::cout << 0;
+	}
+	else {
+		std::cout << fibo(0, 1);
+	}
+}
---
+++ 2447-별 찍기 - 10/2447-별 찍기 - 10.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+void draw(int x, int y, int size) {
+	if ((x / size) % 3 == 1 && (y / size) % 3 == 1)
+		std::cout << ' ';
+	else {
+		if (size / 3 == 0)
+			std::cout << '*';
+		else
+			draw(x, y, size / 3);
+	}
+}
+
+int main() {
+	int n;
+	std::cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			draw(j, i, n);
+		}
+		std::cout << std::endl;
+	}
+	
+}
