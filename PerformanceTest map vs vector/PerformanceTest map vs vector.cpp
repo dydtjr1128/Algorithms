2e6e476aa5598789f6b73c651ff9a35ef7515f07
dydtjr1128 <dydtjr1994@gmail.com>
2020-05-03 19:57:20 +0900

    Add map vs vector performance test

---
+++ PerformanceTest map vs vector/PerformanceTest map vs vector.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <ctime>
+#include <chrono>
+#include <vector>
+#include <unordered_map>
+
+int main() {
+	int size = 1000000;
+	int c = 0;
+	std::vector<int> vector;
+	std::unordered_map<int, int> map;
+
+	srand(time(NULL));
+
+	for (int i = 0; i < size; ++i) {
+		int n = (rand() % 10000) + 1;
+		vector.push_back(n);
+		map.emplace(i, n);
+	}
+
+
+	auto s = std::chrono::system_clock::now();
+	c = 0;
+	for (const auto& n : vector) {
+		c = n;
+	}
+	std::cout << std::chrono::duration<double>(std::chrono::system_clock::now() - s).count() << "ms" << std::endl;
+
+
+	s = std::chrono::system_clock::now();
+	c = 0;
+	for (const auto& f : map) {
+		c = f.second;
+	}
+	std::cout << std::chrono::duration<double>(std::chrono::system_clock::now() - s).count() << "ms" << std::endl;
+
+}
