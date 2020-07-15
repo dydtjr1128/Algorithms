3784bbca5fd2e585a8028712fbc820fdf50370eb
Choi Yong Seok <dydtjr1994@gmail.com>
2020-07-15 19:02:21 +0900

    Add algorithms

---
+++ 1018-체스판 다시 칠하기/1018-체스판 다시 칠하기.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main(){    
+	int n, m;
+	std::cin >> n >> m;
+	int **map = new int*[n];
+	for (int i = 0; i < n; i++) {
+		map[i] = new int[m];
+		for (int j = 0; j < m; j++) {
+			std::cin >> map[i][j];
+		}
+	}
+	
+
+	std::cout << "Hello World!\n";
+}
