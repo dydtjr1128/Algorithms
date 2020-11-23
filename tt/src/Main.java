3ad9474482683e006be88446432fc39143d939d9
Yongseok Choi <dydtjr1994@gmail.com>
2020-11-23 19:01:06 +0900

    Update: 알고리즘 추가.

--- tt/src/Main.java
+++ tt/src/Main.java
@@ -10,25 +10,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
-
         int i = 1;
         int v = 0;
         while (true) {
-            v = calc(i);
-            if (v >= n) {
-                v = calc(i - 1);
+            v = calc(i++);
+            if (v > n) {
+                i--;
+                v = calc(i);
                 break;
-            } else {
-                i++;
             }
         }
         v = n - v - 1;
-
-        if (i % 2 == 0) {
-            System.out.println((1 + v) + "/" + (i - v));
-        } else {
-            System.out.println((i - v) + "/" + (1 + v));
-        }
+        System.out.println((1 + v) + "/" + (i - v));
 
     }
 }
